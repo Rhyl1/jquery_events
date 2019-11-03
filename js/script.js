62ced94fee5ad7d427b179ee29b938a9e32f7777
Rhy <yhr@xs4all.nl>
2019-11-03 13:16:01 +0000

    added course exercise output to files

--- js/script.js
+++ js/script.js
@@ -67,8 +67,80 @@ $(document).ready(function() {
         $("#hr_html").removeClass("h2_font_size");
         $("#hr_css").addClass("h2_font_size");
     });
+//applies colour black to body background when mouse enters over buttons
+    $(".bottom_button").mouseenter(function(){
+        $("body").css( "background-color", "black");
+    });
 
+     //applies colour grey to body background when mouse leaves buttons
+    $(".bottom_button").mouseleave(function(){
+        $("body").css( "background-color", "#eee");
+    });
 
+   // When the button is clicked, the paragraph above vanishes.
+//When the button is clicked again, the paragraph reappears.
+
+    $("#button_effects1").click(function(){
+        $('#par1').slideToggle('1000');
+    });
+    $("#button_effects2").click(function(){
+        $('#par2').toggle('1000');
+    });
+    $("#button_effects3").click(function(){
+        $('#par3').toggle('1000');
+    });
+    $("#button_effects4").click(function(){
+        $('#par4').toggle('1000');
+    });
+    $("#button_effects5").click(function(){
+        $('#par5').toggle('1000');
+    });
+    $("#button_effects6").click(function(){
+        $('#par6').toggle('1000');
+    });
+// Use the fadeTo effect when the mouse hovers over a specific button
+    // and fadeTo again when the mouse is no longer hovering over the button
+    $("#button_effects1").mouseenter(function(){
+        $('#button_effects1').fadeTo(1000, 0.5);
+    });
+    $("#button_effects1").mouseleave(function(){
+        $('#button_effects1').fadeTo(1000, 1);
+    });
+
+    $("#button_effects2").mouseenter(function(){
+        $('#button_effects2').fadeTo(1000, 0.5);
+    });
+    $("#button_effects2").mouseleave(function(){
+        $('#button_effects2').fadeTo(1000, 1);
+    });
+
+    $("#button_effects3").mouseenter(function(){
+        $('#button_effects3').fadeTo(1000, 0.5);
+    });
+    $("#button_effects3").mouseleave(function(){
+        $('#button_effects3').fadeTo(1000, 1);
+    });
+
+    $("#button_effects4").mouseenter(function(){
+        $('#button_effects4').fadeTo(1000, 0.5);
+    });
+    $("#button_effects4").mouseleave(function(){
+        $('#button_effects4').fadeTo(1000, 1);
+    });
+
+    $("#button_effects5").mouseenter(function(){
+        $('#button_effects5').fadeTo(1000, 0.5);
+    });
+    $("#button_effects5").mouseleave(function(){
+        $('#button_effects5').fadeTo(1000, 1);
+    });
+
+    $("#button_effects6").mouseenter(function(){
+        $('#button_effects6').fadeTo(1000, 0.5);
+    });
+    $("#button_effects6").mouseleave(function(){
+        $('#button_effects6').fadeTo(1000, 1);
+    });
 
 
  	$("#stream1_btn").on("click", function() {
